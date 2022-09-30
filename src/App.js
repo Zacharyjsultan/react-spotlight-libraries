f3ceaf1f7a161fd49625987d97f37c7fc9ad23e6
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-30 15:40:21 -0700

    table rendered WOOOHOO

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ShoeTable from './components/ShoeTable.js';
+
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/table">Table</Link>
-        <Link to="/charts">Charts</Link>
+        <Link to="/Table">Table</Link>
+        <Link to="/Charts">Charts</Link>
       </nav>
       <Switch>
-        <Route path="/table">
+        <Route path="/Table">
           <h1>Table Libary</h1>
+          <ShoeTable></ShoeTable>
         </Route>
-        <Route path="/charts">
+        <Route path="/Charts">
           <h1>Chart Library</h1>
         </Route>
       </Switch>
---
+++ src/components/ShoeTable.js
@@ -0,0 +1,29 @@
+import shoe from '../json/shoes.json';
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css';
+
+export default function ShoesTable() {
+  return (
+    <Table data={shoe}>
+      <Column width={100}>
+        <HeaderCell>ID</HeaderCell>
+        <Cell dataKey="id" />
+      </Column>
+
+      <Column width={300}>
+        <HeaderCell>shoe</HeaderCell>
+        <Cell dataKey="shoe" />
+      </Column>
+
+      <Column width={300}>
+        <HeaderCell>company</HeaderCell>
+        <Cell dataKey="company" />
+      </Column>
+
+      <Column width={300}>
+        <HeaderCell>color</HeaderCell>
+        <Cell dataKey="color" />
+      </Column>
+    </Table>
+  );
+}
