24df5f21aade15344c385e401fd1d350381f008d
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-30 17:34:52 -0700

    touches

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -15,7 +15,7 @@ export default function Chart() {
           height={500}
           data={users}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+          <CartesianGrid stroke="#eee" strokeDasharray="1 1"/>
           <XAxis dataKey="id" />
           <Tooltip />
           <Area type="monotone" dataKey="id" stroke="#0000FF" fill="#FFFF00" />
